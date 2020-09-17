ac74c44454de39c5d84a68d13bc1345fb85f5f87
sherrywilly <sherrywilson521@gmail.com>
2020-09-17 21:47:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import "./App.css";
 import Todo from "./Todo";
 
 function App() {
   return (
-    <div>
+    <div className="container-fluid">
       <Todo />
     </div>
   );
